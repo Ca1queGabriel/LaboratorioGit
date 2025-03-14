7b8c5be13671b98fd6faa5924456cfafefeaca7e
CaiqueGabriel <worldcadiiin@gmail.com>
2025-03-14 20:42:27 -0300

    minha mensagem do commit pelo github desktop

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 ## pinguim academy
 
 o readme antigo não fazia sentido nenhum, então consertei ele.
+eu boto ou não boto
--- jeremias.php
+++ jeremias.php
@@ -5,3 +5,4 @@ echo "oi Jeremias";
 echo "estou na main";
 echo "eu boto ou não boto";
 echo "ai eu tô com medo";
+echo "github desktop";
